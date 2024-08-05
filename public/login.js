1c252fc9286ce813c00898f1f49b55d20dc61e1c
Abdul Mohamed <abdul@abdul.com>
2024-08-04 23:47:03 -0700

    removed login into its own html page

---
+++ public/login.js
@@ -0,0 +1,36 @@
+// Logs in the user by sending a request to the backend with the username
+async function login() {
+	
+	const messageElement = document.getElementById('loginResultMsg');
+	messageElement.textContent = "";
+
+	const userID = document.getElementById('userID').value;
+
+	const response = await fetch('/login', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			USERID: userID
+		})
+	});
+
+	const responseData = await response.json();
+
+	if (responseData.success) {
+		localStorage.setItem('LocalStorage-userID', userID);
+		messageElement.textContent = "Login successful!";
+
+		// TODO: if user is admin display all users table and meals and stuff
+		// if user is not admin display only his dashboard page
+		// Redirect to dashboard.html after successful login
+		if (responseData.isAdmin) {
+			window.location.href = 'dashboard_admin.html';
+		} else {
+			window.location.href = 'dashboard.html';
+		}
+	} else {
+		messageElement.textContent = "Login failed!";
+	}
+}
